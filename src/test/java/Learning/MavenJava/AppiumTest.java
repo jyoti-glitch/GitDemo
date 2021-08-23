b91b87c5571caec33a181602abb5cb11b02c08f0
jyoti-glitch <jyotiranjanm.b.eng@gmail.com>
2021-08-23 23:03:19 +0530

    first commit

---
+++ src/test/java/Learning/MavenJava/AppiumTest.java
@@ -0,0 +1,22 @@
+package Learning.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+		
+		@Test
+		public void NativeAppAndroid()
+		{
+			System.out.println("NativeAppAndroid");
+		}
+		
+		@Test
+		public void IOSApps()
+		{
+			System.out.println("IOSApps");
+
+		}
+	}
+
+
---
+++ src/test/java/Learning/MavenJava/SeleniumTest.java
@@ -0,0 +1,19 @@
+package Learning.MavenJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void postJira() {
+		System.out.println("postJira");
+
+	}
+	@Test
+	public void apiSelenium() {
+		System.out.println("apiSelenium");
+
+	}
+
+}
